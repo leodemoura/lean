361f39450e114de88aa3693988550e16bd489c06
Leonardo de Moura <leonardo@microsoft.com>
2017-09-08 17:11:50 -0700

    example

---
+++ tests/lean/run/1804b.lean
@@ -0,0 +1,26 @@
+namespace bla
+
+private def foo (x : nat) : nat :=
+match x with
+| 0 := 1
+| _ := 2
+end
+
+example (a : nat) (h : a = foo 0) : a = 1 :=
+begin
+  simp [foo] at h,
+  guard_hyp h := a = 1,
+  exact h
+end
+
+example (a b : nat) (p : b = 0) (h : a = foo b) : a = 1 :=
+begin
+  simp [foo] at h,
+  simp [p] at h,
+  -- TODO(Leo):
+  -- simp [foo._match_1] does not work. We are not creating the alias to the private name.
+  simp [foo] at h,
+  guard_hyp h := a = 1,
+  exact h
+end
+end bla
